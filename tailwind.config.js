5ff330b973ca0a4f9dff9a64b93b8e05142ab390
dboudro <dylanrboudro@gmail.com>
2020-11-01 10:59:40 -0700

    change tailwind config file colors to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,34 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sf-blue': {
+          50: '#E1EDF5',
+          100: '#C3DBEB',
+          200: '#A5CAE0',
+          300: '#87B8D6',
+          400: '#69A6CC',
+          500: '#4B94C2',
+          600: '#3A80AB',
+          700: '#30698C',
+          800: '#2A5C7B',
+          900: '#244F69',
+          1000: '#1E4258',
+          1100: '#183546',
+          1200: '#122735',
+          1300: '#0C1A23',
+          },
+          'sf-orange': {
+            100: '#E46D53',
+          },
+          'sf-gold': {
+            100: '#CF934A',
+          }  
+      },
+
+    },
   },
-  variants: {
-  	backgroundColor: ['responsive', 'hover', 'focus', 'active']
-  },
+  variants: {},
   plugins: [],
 }
